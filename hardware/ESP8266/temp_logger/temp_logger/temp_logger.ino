ec79dd0b3f4d900ac3d56ac02373dc20bd5f5247
Ardbot <45563576+Ardbot@users.noreply.github.com>
2022-02-15 15:57:26 +0900

    Update temp_logger.ino
    Разбивка по функциям

--- hardware/ESP8266/temp_logger/temp_logger/temp_logger.ino
+++ hardware/ESP8266/temp_logger/temp_logger/temp_logger.ino
@@ -1,6 +1,5 @@
 #include <ESP8266WiFi.h>
 #include "ThingSpeak.h"
-#include <Adafruit_Sensor.h>
 
 const char* ssid = "Xiaomi_2CC8";   // Имя WI-FI сети (SSID)
 const char* password = "2styKHDQ";   // Пароль от WI-FI
@@ -27,39 +26,55 @@ void setup() {
 
 void loop() {
   if ((millis() - lastTime) > timerDelay) { // Если время пауза прошла
-
-    Serial.print(WIFI_connect());
-    if ( WIFI_connect() == 1) {
-
-    // Пока выставляем рандом
-    temperatureC = random(-40, 40);
-      Serial.print("Temperature (ºC): ");
-      Serial.println(temperatureC);
-
-      int x = ThingSpeak.writeField(myChannelNumber, 1, temperatureC, myWriteAPIKey);
-
-      if (x == 200) { // Если ответ с сервера 200
-        Serial.println("Channel update successful.");
-      }
-      else {
-        Serial.println("Problem updating channel. HTTP error code " + String(x));
-      }
-      lastTime = millis();
+    if (WIFI_connect() == 1) {
+      post_temp();
+    }
+    else {
+      WIFI_connect();
     }
   }
 }
 
-
 byte WIFI_connect() {   // Подключаемся к WiFi
   if (WiFi.status() != WL_CONNECTED) {
-    Serial.print("Error connect 0");
-    while (WiFi.status() != WL_CONNECTED) {
+    Serial.print("Connection to ");
+    Serial.println(ssid);
+    int i = 0;
+    while (WiFi.status() != WL_CONNECTED && i < 5) {
       WiFi.begin(ssid, password);
-      Serial.print(".");
+      i++;
+      Serial.print("Connection attempt:");
+      Serial.println(i);
       delay(5000);
     }
-    Serial.println("\nConnected.");
+    if (WiFi.status() != WL_CONNECTED) {
+      Serial.println("Status: disconnect");
+      return 0;
+    }
+    else {
+      Serial.println("Status: connect");
+      return 1;
+    }
+  }
+  else return 1;
+
+}
+
+byte post_temp() {  // Отправка данных
+  // Пока выставляем рандом
+  temperatureC = random(-40, 40);
+  Serial.print("Temperature (ºC): ");
+  Serial.println(temperatureC);
+
+  int x = ThingSpeak.writeField(myChannelNumber, 1, temperatureC, myWriteAPIKey);
+
+  if (x == 200) { // Если ответ с сервера 200
+    Serial.println("Channel update successful.");
     return 1;
   }
-
+  else {
+    Serial.println("Problem updating channel. HTTP error code " + String(x));
+    return 0;
+  }
+  lastTime = millis();
 }
