98bd936b1e094e36714206722b01988d34fbece2
Ardbot <45563576+Ardbot@users.noreply.github.com>
2022-04-03 14:03:53 +0900

    Логгер и виджет
    Обновление логгера температуры и добавление виджета на страницу "Умного сада"

--- hardware/ESP8266/temp_logger/temp_logger/temp_logger.ino
+++ hardware/ESP8266/temp_logger/temp_logger/temp_logger.ino
@@ -1,80 +1,79 @@
 #include <ESP8266WiFi.h>
-#include "ThingSpeak.h"
+#include "config.h"
+#include "ThingSpeak.h" // always include thingspeak header file after other header files and custom macros
 
-const char* ssid = "Xiaomi_2CC8";   // Имя WI-FI сети (SSID)
-const char* password = "2styKHDQ";   // Пароль от WI-FI
+#include <microDS18B20.h>
+MicroDS18B20<4> sensor;
+//MicroDS18B20<5> sensor2;
 
+char ssid[] = SECRET_SSID;   // your network SSID (name)
+char pass[] = SECRET_PASS;   // your network password
+int keyIndex = 0;            // your network key Index number (needed only for WEP)
 WiFiClient  client;
 
-unsigned long myChannelNumber = 1384395;  // Номер канала
-const char * myWriteAPIKey = "ETSGVZ600T5AWTH7";  // Ключ канала
+unsigned long myChannelNumber = SECRET_CH_ID;
+const char * myWriteAPIKey = SECRET_WRITE_APIKEY;
 
-// Время между отправкой сообщения
-unsigned long lastTime = 0;
-unsigned long timerDelay = 20000;
-
-// Переменная для храниния температуры
-float temperatureC;
-
+String myStatus = "";
 
 void setup() {
-  Serial.begin(115200);  //Инициализируем последовательный порт
-  WiFi.mode(WIFI_STA);
+  Serial.begin(115200);  // Initialize serial
+  while (!Serial) {
+    ; // wait for serial port to connect. Needed for Leonardo native USB port only
+  }
 
-  ThingSpeak.begin(client);  // Инициализируем ThingSpeak
+  WiFi.mode(WIFI_STA);
+  ThingSpeak.begin(client);  // Initialize ThingSpeak
 }
 
 void loop() {
-  if ((millis() - lastTime) > timerDelay) { // Если время пауза прошла
-    if (WIFI_connect() == 1) {
-      post_temp();
-    }
-    else {
-      WIFI_connect();
-    }
-  }
-}
 
-byte WIFI_connect() {   // Подключаемся к WiFi
+  // Connect or reconnect to WiFi
   if (WiFi.status() != WL_CONNECTED) {
-    Serial.print("Connection to ");
-    Serial.println(ssid);
-    int i = 0;
-    while (WiFi.status() != WL_CONNECTED && i < 5) {
-      WiFi.begin(ssid, password);
-      i++;
-      Serial.print("Connection attempt:");
-      Serial.println(i);
+    Serial.print("Connect to SSID: ");
+    Serial.println(SECRET_SSID);
+    while (WiFi.status() != WL_CONNECTED) {
+      WiFi.begin(ssid, pass);  // Connect to WPA/WPA2 network. Change this line if using open or WEP network
+      Serial.print(".");
+      Serial.println(WiFi.status());
       delay(5000);
     }
-    if (WiFi.status() != WL_CONNECTED) {
-      Serial.println("Status: disconnect");
-      return 0;
-    }
-    else {
-      Serial.println("Status: connect");
-      return 1;
-    }
+    Serial.println("\nConnected.");
   }
-  else return 1;
+  sensor.requestTemp();
+//  sensor2.requestTemp();
+  delay(1000);
+  float temp1 = sensor.getTemp();
+//  int temp2 = sensor2.getTemp();
 
-}
+  // set the fields with the values
+  ThingSpeak.setField(1, temp1);
+//  ThingSpeak.setField(2, temp2);
 
-byte post_temp() {  // Отправка данных
-  // Пока выставляем рандом
-  temperatureC = random(-40, 40);
-  Serial.print("Temperature (ºC): ");
-  Serial.println(temperatureC);
+  // figure out the status message
+  if (temp1<1) {
+    myStatus = String("Заморозки " + String(temp1));
+  }
+    else if (temp1<15) {
+    myStatus = String("Прохладно "  + String(temp1));
+  }
+  else if (temp1>30) {
+    myStatus = String("Жарко " + String(temp1));
+  }
+  else {
+    myStatus = String("Температура норм "  + String(temp1));
+  }
 
-  int x = ThingSpeak.writeField(myChannelNumber, 1, temperatureC, myWriteAPIKey);
+  // set the status
+  ThingSpeak.setStatus(myStatus);
 
-  if (x == 200) { // Если ответ с сервера 200
+  // write to the ThingSpeak channel
+  int x = ThingSpeak.writeFields(myChannelNumber, myWriteAPIKey);
+  if (x == 200) {
     Serial.println("Channel update successful.");
-    return 1;
   }
   else {
     Serial.println("Problem updating channel. HTTP error code " + String(x));
-    return 0;
   }
-  lastTime = millis();
+  delay(300000); // Wait 300 seconds to update the channel again
 }
